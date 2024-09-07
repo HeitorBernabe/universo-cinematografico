969e93c91874f911c6e17345531539a89af7e457
HeitorBernabe <heitor.bernabe34@gmail.com>
2024-09-07 18:25:07 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,21 @@
+let dados = [
+    {
+        titulo : "Meu Malvado Favorito 4",
+        descricao : "Em Meu Malvado Favorito 4, Gru, o ex-supervilão mais querido do mundo, agora um agente da Liga Anti-Vilões, enfrenta um novo desafio. Com a chegada do pequeno Gru Jr., a vida já está bem agitada. Mas a paz é interrompida quando um antigo inimigo, Maxime Le Mal, escapa da prisão e ameaça a segurança de toda a família. Acompanhe Gru, Lucy e os adoráveis Minions em mais uma aventura repleta de humor, ação e muita emoção!",
+        link : "https://www.universalpics.com.br/micro/dm4",
+        tags : "engraçado engracado recente novo filme lançamento 2024"
+    }, 
+    {
+        titulo : "Sonic 3",
+        descricao : "O ouriço azul mais rápido do mundo está de volta em uma nova aventura épica! Sonic, Tails e Knuckles se unem para enfrentar um novo vilão poderoso e proteger o mundo de uma ameaça intergaláctica.",
+        link : "https://www.paramountpictures.com/br/movies/sonic-the-hedgehog-3",
+        tags : "ouriço azul ourico shadow novo filme recente 2024 lançamento"
+    }, 
+    {
+        titulo : "O Auto da Compadecida 2",
+        descricao : "A continuação da clássica história de Chico Bento e João Grilo promete mais uma aventura repleta de humor, música e críticas sociais. Nesta nova jornada, a dupla de amigos se envolve em uma trama emocionante que promete divertir todas as idades.",
+        link : "https://www.seusite.com.br/oautodacompadecida2",
+        tags : "chicó chico chapeu ariano suassuna suaçuna grilos grilo filme dezembro novo 2024 25"
+    }
+];
+
